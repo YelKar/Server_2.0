194bc0596ffee1b5bb807b74768c874903c34ea3
YelKar <karamyshev_e@1060.ru>
2022-02-08 07:26:45 +0300

    add flask_wtf

--- app/forms.py
+++ app/forms.py
@@ -4,9 +4,15 @@ from wtforms.validators import DataRequired, Email
 
 
 class Form(FlaskForm):
-    name = StringField("Логин: ", validators=[DataRequired()], render_kw={"placeholder": "Введите имя"})
-    password = PasswordField("Пароль: ", validators=[DataRequired()], render_kw={"placeholder": "Введите пароль"})
+    name = StringField("Логин: *", validators=[DataRequired()], render_kw={"placeholder": "Введите имя"})
+    password = PasswordField("Пароль: *", validators=[DataRequired()], render_kw={"placeholder": "Введите пароль"})
     email = EmailField("Email: ", validators=[Email()], render_kw={"placeholder": "Введите вашу почту"})
-    remember = BooleanField('Запомнить:', default='checked')
+    remember = BooleanField('Запомнить', default='checked')
     submit = SubmitField("Отправить")
 
+
+class LoginForm(FlaskForm):
+    name = StringField("Логин: *", validators=[DataRequired()], render_kw={"placeholder": "Введите имя"})
+    password = PasswordField("Пароль: *", validators=[DataRequired()], render_kw={"placeholder": "Введите пароль"})
+    remember = BooleanField('Запомнить', default='checked')
+    submit = SubmitField("Отправить")
